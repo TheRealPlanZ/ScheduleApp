decedfed4d36ff7b3efb8df18bb9dff25ac238ed
Zack Levin <zacklevin@gmail.com>
2022-10-18 18:51:44 -0700

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -15,19 +15,19 @@ $(document).ready(function () {
    
     function scheduler() {
         //get current number of hours.
-        var timeNow = moment().hour();
+        var currentTime = moment().hour();
 
         // loop over time blocks
         $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
             // To check the time and add the classes for background indicators
-            if (blockTime < timeNow) {
+            if (blockTime < currentTime) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past");
             }
-            else if (blockTime === timeNow) {
+            else if (blockTime === currentTime) {
                 $(this).removeClass("past");
                 $(this).removeClass("future");
                 $(this).addClass("present");
